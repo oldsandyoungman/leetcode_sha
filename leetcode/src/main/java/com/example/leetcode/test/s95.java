58d97ad9ca634a6c739db0c6f74239773116be35
oldsandyoungman <874997364@qq.com>
2021-08-30 20:48:09 +0800

    二叉搜索树问题做了点题

--- leetcode/src/main/java/com/example/leetcode/test/s95.java
+++ leetcode/src/main/java/com/example/leetcode/test/s95.java
@@ -13,31 +13,58 @@ public class s95 {
 
 //    给你一个整数 n ，请你生成并返回所有由 n 个节点组成且节点值从 1 到 n 互不相同的不同 二叉搜索树 。可以按 任意顺序 返回答案。
 
+//    public static List<TreeNode> generateTrees(int n) {
+//        return build(1,n);
+//    }
+//
+////    public static List<TreeNode> sum = new ArrayList<>();
+//
+//    public static List<TreeNode> build(int lo, int hi){
+//        List<TreeNode> res = new ArrayList<>();
+//
+//        if (lo>hi) {
+//            res.add(null);
+//            return res;
+//        }
+//
+//        for (int i = lo; i <= hi; i++) {
+//
+//            List<TreeNode> left = build(lo, i-1);
+//            List<TreeNode> right = build(i+1, hi);
+//
+//            for (TreeNode lefttreeNode : left) {
+//                for (TreeNode righttreenode : right) {
+//
+//                    TreeNode root = new TreeNode(i);
+//                    root.left = lefttreeNode;
+//                    root.right = righttreenode;
+//                    res.add(root);
+//                }
+//            }
+//        }
+//
+//        return res;
+//
+//    }
+
     public static List<TreeNode> generateTrees(int n) {
         return build(1,n);
     }
 
-//    public static List<TreeNode> sum = new ArrayList<>();
-
     public static List<TreeNode> build(int lo, int hi){
         List<TreeNode> res = new ArrayList<>();
-
         if (lo>hi) {
-            res.add(null);
             return res;
         }
 
         for (int i = lo; i <= hi; i++) {
-
             List<TreeNode> left = build(lo, i-1);
             List<TreeNode> right = build(i+1, hi);
-
-            for (TreeNode lefttreeNode : left) {
-                for (TreeNode righttreenode : right) {
-
+            for (TreeNode l : left) {
+                for (TreeNode r : right) {
                     TreeNode root = new TreeNode(i);
-                    root.left = lefttreeNode;
-                    root.right = righttreenode;
+                    root.left = l;
+                    root.right = r;
                     res.add(root);
                 }
             }
@@ -45,6 +72,9 @@ public class s95 {
 
         return res;
 
+
+
     }
+
 
 }
--- leetcode/src/main/java/com/example/leetcode/test/s96.java
+++ leetcode/src/main/java/com/example/leetcode/test/s96.java
@@ -10,34 +10,68 @@ public class s96 {
 
 //    给你一个整数 n ，求恰由 n 个节点组成且节点值从 1 到 n 互不相同的 二叉搜索树 有多少种？返回满足题意的二叉搜索树的种数。
 
+//    public static int[] memo;
+//
+//    public static int numTrees(int n) {
+//        memo = new int[n];
+//        return count(1, n);
+//    }
+//
+//    public static int count(int lo, int hi){
+//
+//        if (lo>hi) {
+//            return 1;
+//        }
+//
+//        if (memo[hi-lo]!=0) {
+//            return memo[hi-lo];
+//        }
+//
+//        int res =0;
+//        for (int i = lo; i <= hi; i++) {
+//            int left = count(lo, i-1);
+//            int right = count(i+1, hi);
+//            res += left * right;
+//        }
+//
+//        memo[hi-lo] = res;
+//        return res;
+//
+//    }
+
+
+
+
+
     public static int[] memo;
 
     public static int numTrees(int n) {
         memo = new int[n];
-        return count(1, n);
+        return count(1,n);
     }
 
     public static int count(int lo, int hi){
-
-        if (lo>hi) {
+        if (lo>hi){
             return 1;
         }
-
         if (memo[hi-lo]!=0) {
             return memo[hi-lo];
         }
-
-        int res =0;
+        int sum = 0;
         for (int i = lo; i <= hi; i++) {
             int left = count(lo, i-1);
             int right = count(i+1, hi);
-            res += left * right;
+            sum += left * right;
         }
-
-        memo[hi-lo] = res;
-        return res;
-
+        memo[hi-lo] = sum;
+        return sum;
     }
+
+
+
+
+
+
 
 
 //    public static int count(int lo, int hi){
