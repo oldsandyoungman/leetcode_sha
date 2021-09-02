9a660b4b9b85b87ea62807895c21e3c62590262f
oldsandyoungman <874997364@qq.com>
2021-09-02 22:51:36 +0800

    推特朋友圈设计问题做了点题

--- leetcode/src/main/java/com/example/leetcode/test/s355.java
+++ leetcode/src/main/java/com/example/leetcode/test/s355.java
@@ -146,3 +146,45 @@ class Twitter {
         }
     }
 }
+
+
+
+//class Twitter {
+//
+//    static int timestamp = 0;
+//    static class Tweet{
+//
+//    }
+//    static class User{
+//
+//    }
+//
+//    static HashMap<Integer, User> map = new HashMap<>();
+//
+//    /** Initialize your data structure here. */
+//    public Twitter() {
+//
+//    }
+//
+//    /** Compose a new tweet. */
+//    public void postTweet(int userId, int tweetId) {
+//
+//    }
+//
+//    /** Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent. */
+//    public List<Integer> getNewsFeed(int userId) {
+//
+//    }
+//
+//    /** Follower follows a followee. If the operation is invalid, it should be a no-op. */
+//    public void follow(int followerId, int followeeId) {
+//
+//    }
+//    /** Follower unfollows a followee. If the operation is invalid, it should be a no-op. */
+//    public void unfollow(int followerId, int followeeId) {
+//
+//    }
+//
+//
+//
+//}
