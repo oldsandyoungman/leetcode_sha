87f9b22875f8ff0edf4193eb0429972be2c0dd77
oldsandyoungman <874997364@qq.com>
2021-09-04 19:03:48 +0800

    推特朋友圈设计问题做了点题

---
+++ leetcode/src/main/java/com/example/leetcode/labuladong/next_greater_number.java
@@ -0,0 +1,54 @@
+package com.example.leetcode.labuladong;
+
+import java.util.*;
+
+public class next_greater_number {
+
+    public static void main(String[] args) {
+
+        int[] s = new int[]{2,1,2,4,3};
+
+        System.out.println(Arrays.toString(nextGreaterElement(s)));
+        System.out.println(Arrays.toString(nextGreaterElement2(s)));
+
+    }
+
+    private static int[] nextGreaterElement(int[] nums) {
+        int[] res = new int[nums.length];
+        Stack<Integer> s = new Stack<>();
+        // 倒着往栈里放
+        for (int i = nums.length - 1; i >= 0; i--) {
+            // 判定个子高矮
+            while (!s.empty() && s.peek() <= nums[i]) {
+                // 矮个起开，反正也被挡着了。。。
+                s.pop();
+            }
+            // nums[i] 身后的 next great number
+            res[i] = s.empty() ? -1 : s.peek();
+            s.push(nums[i]);
+        }
+        return res;
+    }
+
+
+
+    private static int[] nextGreaterElement2(int[] nums) {
+        int[] res = new int[nums.length];
+        Stack<Integer> s = new Stack<>();
+
+        for (int i = nums.length-1; i >= 0; i--){
+
+            while (!s.empty() && s.peek() <= nums[i]){
+                s.pop();
+            }
+            res[i] = s.empty()? -1 : s.peek();
+            s.push(nums[i]);
+        }
+
+        return res;
+
+
+    }
+
+
+}
