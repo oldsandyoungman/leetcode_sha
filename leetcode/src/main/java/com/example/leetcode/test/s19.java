90e8641480baf8d28b219d800911ceae0628b1a1
oldsandyoungman <874997364@qq.com>
2021-09-14 12:26:22 +0800

    快慢指针问题做了点题

--- leetcode/src/main/java/com/example/leetcode/test/s19.java
+++ leetcode/src/main/java/com/example/leetcode/test/s19.java
@@ -25,67 +25,96 @@ public class s19 {
         System.out.println(a);
 //        System.out.println(removeNthFromEnd2(a,5));
 //        System.out.println(removeNthFromEnd2(a,6));
-        System.out.println(removeNthFromEnd2(a,1));
+        System.out.println(removeNthFromEnd(a,1));
 
     }
 //    给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
 //
 //    进阶：你能尝试使用一趟扫描实现吗？
 
+//    public static ListNode removeNthFromEnd(ListNode head, int n) {
+//
+//        ListNode l1 = head;
+//        ListNode l2 = head;
+//
+//        for (int i = 0; i < n+1; i++) {
+//            if(l1.next != null){
+//                l1=l1.next;
+//            }else{
+//                head = head.next;
+//                return head;
+//            }
+//
+//        }
+//        while (l1!=null){
+//            l1 = l1.next;
+//            l2 = l2.next;
+//        }
+//
+//        l2.next = l2.next.next;
+//
+//        return head;
+//
+//
+//    }
+//
+//    public static ListNode removeNthFromEnd2(ListNode head, int n) {
+//
+//        ListNode l1 = head;
+//        ListNode l2 = head;
+//        ListNode pre = null;
+//
+//        for (int i = 0; i < n; i++) {
+//            if(l1.next != null){
+//                l1 = l1.next;
+//            }else {
+//                return head.next;
+//            }
+//        }
+//        while (l1!=null){
+//            if(l2==head){
+//                pre = head;
+//            }else {
+//                pre = pre.next;
+//            }
+//            l1 = l1.next;
+//            l2 = l2.next;
+//        }
+//
+//        pre.next = l2.next;
+//
+//        return head;
+//
+//
+//    }
+
     public static ListNode removeNthFromEnd(ListNode head, int n) {
-
-        ListNode l1 = head;
-        ListNode l2 = head;
-
-        for (int i = 0; i < n+1; i++) {
-            if(l1.next != null){
-                l1=l1.next;
-            }else{
-                head = head.next;
-                return head;
-            }
-
-        }
-        while (l1!=null){
-            l1 = l1.next;
-            l2 = l2.next;
+        if (head==null){
+            return null;
         }
 
-        l2.next = l2.next.next;
-
-        return head;
-
-
-    }
-
-    public static ListNode removeNthFromEnd2(ListNode head, int n) {
-
-        ListNode l1 = head;
-        ListNode l2 = head;
-        ListNode pre = null;
+        ListNode slow = head;
+        ListNode fast = head;
 
         for (int i = 0; i < n; i++) {
-            if(l1.next != null){
-                l1 = l1.next;
-            }else {
-                return head.next;
-            }
+            fast = fast.next;
         }
-        while (l1!=null){
-            if(l2==head){
-                pre = head;
-            }else {
-                pre = pre.next;
-            }
-            l1 = l1.next;
-            l2 = l2.next;
+        if (fast==null){
+            return head.next;
         }
 
-        pre.next = l2.next;
+        while (fast.next!=null){
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        slow.next = slow.next.next;
 
         return head;
 
 
+
     }
+
 
 }
