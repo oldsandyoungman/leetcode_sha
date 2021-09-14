dc71a0997a48f01a0fe7722baacc418809ffecb1
oldsandyoungman <874997364@qq.com>
2021-09-14 15:53:08 +0800

    滑动窗口问题做了点题

--- leetcode/src/main/java/com/example/leetcode/test/s567.java
+++ leetcode/src/main/java/com/example/leetcode/test/s567.java
@@ -9,13 +9,17 @@ public class s567 {
         //        String a = "ADOBECODEBANC";
 //        String a = "A";
 //        String a = "ab";
-        String a = "a";
+//        String a = "a";
 //        String a = "adc";
+        String a = "ab";
+
+
 //        String t = "ABC";
 //        String t = "A";
 //        String t = "eidboooab";
 //        String t = "dcda";
-        String t = "ab";
+//        String t = "ab";
+        String t = "a";
 
         System.out.println(checkInclusion(a,t));
     }
@@ -75,65 +79,135 @@ public class s567 {
 //
 //    }
 
-    public static boolean checkInclusion(String s1, String s2) {
+//    public static boolean checkInclusion(String s1, String s2) {
+//
+//        if (s1.length()>s2.length()){
+//            return false;
+//        }
+//
+//        Map<Character,Integer> need = new HashMap<>();
+//        Map<Character,Integer> window = new HashMap<>();
+//
+//        char[] s1_array = s1.toCharArray();
+//        for (char c : s1_array) {
+//            need.put(c,need.getOrDefault(c,0)+1);
+//        }
+//
+//        int left = 0;
+//        int right = 0;
+//        int valid = 0;
+//
+//        while (right<s1.length()){
+//
+//            char r = s2.charAt(right);
+//            if (need.containsKey(r)){
+//                window.put(r,window.getOrDefault(r,0)+1);
+//                if (window.get(r).equals(need.get(r))){
+//                    valid++;
+//                }
+//            }
+//            right++;
+//        }
+//
+//        if (valid==need.size()){
+//            return true;
+//        }
+//
+//        while (right<s2.length()) {
+//
+//            char r = s2.charAt(right);
+//            if (need.containsKey(r)){
+//                window.put(r,window.getOrDefault(r,0)+1);
+//                if (window.get(r).equals(need.get(r))){
+//                    valid++;
+//                }
+//            }
+//            right++;
+//
+//            char l = s2.charAt(left);
+//            if (need.containsKey(l)){
+//                window.put(l,window.get(l)-1);
+//                if (window.get(l)==need.get(l)-1) {
+//                    valid--;
+//                }
+//            }
+//
+//            left++;
+//
+//            if (valid==need.size()){
+//                return true;
+//            }
+//
+//
+//        }
+//
+//        return false;
+//
+//
+//    }
 
-        if (s1.length()>s2.length()){
+
+    public static boolean checkInclusion(String s1, String s2) {
+        if (s1.length()>s2.length()) {
             return false;
         }
 
-        Map<Character,Integer> need = new HashMap<>();
-        Map<Character,Integer> window = new HashMap<>();
+        HashMap<Character, Integer> need = new HashMap<>();
 
-        char[] s1_array = s1.toCharArray();
-        for (char c : s1_array) {
-            need.put(c,need.getOrDefault(c,0)+1);
+        char[] s1_chars = s1.toCharArray();
+
+        for (char s1_char : s1_chars) {
+            need.put(s1_char, need.getOrDefault(s1_char, 0)+1);
         }
+
+        HashMap<Character, Integer> window = new HashMap<>();
 
         int left = 0;
         int right = 0;
+        int res_l = 0;
+        int res_r = 0;
         int valid = 0;
 
-        while (right<s1.length()){
-
-            char r = s2.charAt(right);
-            if (need.containsKey(r)){
-                window.put(r,window.getOrDefault(r,0)+1);
-                if (window.get(r).equals(need.get(r))){
+        for (int i = 0; i < s1.length(); i++) {
+            char tmp = s2.charAt(right);
+            if (need.containsKey(tmp)) {
+                window.put(tmp, window.getOrDefault(tmp, 0)+1);
+                if (window.get(tmp).equals(need.get(tmp))) {
                     valid++;
                 }
             }
             right++;
         }
-
-        if (valid==need.size()){
+        if (valid==need.size()) {
             return true;
         }
 
-        while (right<s2.length()) {
 
-            char r = s2.charAt(right);
-            if (need.containsKey(r)){
-                window.put(r,window.getOrDefault(r,0)+1);
-                if (window.get(r).equals(need.get(r))){
+        while (right<s2.length()) {
+            char tmp = s2.charAt(right);
+            if (need.containsKey(tmp)) {
+                window.put(tmp, window.getOrDefault(tmp, 0)+1);
+                if (window.get(tmp).equals(need.get(tmp))) {
                     valid++;
                 }
             }
+
             right++;
 
-            char l = s2.charAt(left);
-            if (need.containsKey(l)){
-                window.put(l,window.get(l)-1);
-                if (window.get(l)==need.get(l)-1) {
+            tmp = s2.charAt(left);
+            if (need.containsKey(tmp)) {
+                window.put(tmp, window.getOrDefault(tmp, 0)-1);
+                if (window.get(tmp).equals(need.get(tmp)-1)) {
                     valid--;
                 }
             }
 
             left++;
 
-            if (valid==need.size()){
+
+            if (valid==need.size()) {
                 return true;
             }
-
 
         }
 
@@ -141,5 +215,7 @@ public class s567 {
 
 
     }
+
+
 
 }
