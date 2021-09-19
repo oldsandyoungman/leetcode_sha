2d57729a95deccdd069f02b83e805108afa1d035
oldsandyoungman <874997364@qq.com>
2021-09-19 23:16:44 +0800

    动态规划问题做了点题

---
+++ leetcode/src/main/java/com/example/leetcode/test/s72.java
@@ -0,0 +1,53 @@
+package com.example.leetcode.test;
+
+public class s72 {
+
+    public static void main(String[] args) {
+
+    }
+
+
+//    给你两个单词 word1 和 word2，请你计算出将 word1 转换成 word2 所使用的最少操作数 。
+//
+//    你可以对一个单词进行如下三种操作：
+//
+//    插入一个字符
+//            删除一个字符
+//    替换一个字符
+// 
+//
+//    来源：力扣（LeetCode）
+//    链接：https://leetcode-cn.com/problems/edit-distance
+//    著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+    // dp table 方法
+    public static int minDistance(String word1, String word2) {
+        int n1 = word1.length();
+        int n2 = word2.length();
+
+        int[][] dp = new int[n1+1][n2+1];
+
+        for (int i = 0; i <= n1; i++) {
+            dp[i][0] = i;
+        }
+        for (int i = 0; i <= n2; i++) {
+            dp[0][i] = i;
+        }
+
+
+        for (int i = 1; i <= n1; i++) {
+            for (int j = 1; j <= n2; j++) {
+                if (word1.charAt(i)==word2.charAt(j)) {
+                    dp[i][j] = dp[i-1][j-1];
+                }else {
+                    dp[i][j] = 1 + Math.min(dp[i-1][j], Math.min(dp[i][j-1], dp[i-1][j-1]));
+                }
+            }
+        }
+
+        return dp[n1][n2];
+
+    }
+
+
+}
