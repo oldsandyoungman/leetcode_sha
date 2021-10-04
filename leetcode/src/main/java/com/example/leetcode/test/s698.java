133ec011f923ae1dd6585681178480d1ef454729
oldsandyoungman <874997364@qq.com>
2021-10-04 22:50:43 +0800

    回溯算法之集合划分问题做了点题

--- leetcode/src/main/java/com/example/leetcode/test/s698.java
+++ leetcode/src/main/java/com/example/leetcode/test/s698.java
@@ -59,5 +59,50 @@ public class s698 {
         return false;
     }
 
+    // 每个桶来选择数
+    public static boolean canPartitionKSubsets(int[] nums, int k) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        int[] nums_r = new int[n];
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            nums_r[i] = nums[n-1-i];
+            sum += nums[i];
+        }
+        if (sum%k!=0) {
+            return false;
+        }
+        int target = sum/k;
+        int[] buckets = new int[k];
+        return backtrack(nums_r, 0, k, target, buckets);
+
+    }
+
+    public static boolean backtrack(int[] nums, int start, int k, int target, int[] buckets){
+        if (start == nums.length) {
+            for (int i = 0; i < k; i++) {
+                if (buckets[i]!=target){
+                    return false;
+                }
+            }
+            return true;
+        }
+        for (int i = 0; i < k; i++) {
+            if (buckets[i] + nums[start] > target){
+                continue;
+            }
+            buckets[i] += nums[start];
+            boolean flag = backtrack(nums, start+1, k, target, buckets);
+            if (flag) {
+                return true;
+            }
+            buckets[i] -= nums[start];
+        }
+
+        return false;
+    }
+
+
+
 
 }
