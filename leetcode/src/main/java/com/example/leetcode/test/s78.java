e78c5c400172294f413b5dad4060997122fde5cd
oldsandyoungman <874997364@qq.com>
2021-10-07 12:28:43 +0800

    回溯算法之子集问题做了点题

---
+++ leetcode/src/main/java/com/example/leetcode/test/s78.java
@@ -0,0 +1,52 @@
+package com.example.leetcode.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class s78 {
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1,2,3};
+        System.out.println(subsets(nums));
+    }
+
+//    给你一个整数数组 nums ，数组中的元素 互不相同 。返回该数组所有可能的子集（幂集）。
+//
+//    解集 不能 包含重复的子集。你可以按 任意顺序 返回解集。
+
+    public static List<List<Integer>> subsets(int[] nums) {
+        res = new ArrayList<>();
+        List<Integer> now = new ArrayList<>();
+        backtrack(nums, 0, now);
+        return res;
+    }
+    public static List<List<Integer>> res;
+//    public static void backtrack(int[] nums, int start, List<Integer> now){
+//        if (start== nums.length) {
+//            res.add(new ArrayList<>(now));
+//            return;
+//        }
+//
+//        now.add(nums[start]);
+//        backtrack(nums, start+1, now);
+//
+//        now.remove(now.size()-1);
+//        backtrack(nums, start+1, now);
+//
+//    }
+    public static void backtrack(int[] nums, int start, List<Integer> now){
+//        if (start== nums.length) {
+//            res.add(new ArrayList<>(now));
+//            return;
+//        }
+        res.add(new ArrayList<>(now));
+
+        for (int i = start; i < nums.length; i++) {
+            now.add(nums[i]);
+            backtrack(nums, i+1, now);
+            now.remove(now.size()-1);
+        }
+
+    }
+
+}
